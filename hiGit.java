e2757fec40a61dd8dfddd7feaa80a2a9bebb98d1
dead-Atlas <erfanrahmat01@gmail.com>
2025-04-09 11:36:09 +0330

    Edit hiGit.java

--- hiGit.java
+++ hiGit.java
@@ -1,5 +1,9 @@
 
-
 public class hiGit {
-
+    public static void main(String[] args) {
+        System.out.println("Hi my git!");
+        System.out.println("Hi my git!");
+        System.out.println("Hi my git!");
+    }
 }
+
